194c4e1e42a78550108af1b77d79d96c4add9306
Vadym Kuzmyk <vkuzmyk@e1r5p16.unit.ua>
2017-11-15 21:57:04 +0200

    wolf3d

---
+++ Wold3d/libft/ft_lstpushback.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstpushback.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkuzmyk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/23 14:11:27 by vkuzmyk           #+#    #+#             */
+/*   Updated: 2017/04/23 14:11:31 by vkuzmyk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstpushback(t_list **blst, void const *content,
+		size_t content_size)
+{
+	t_list	*list;
+
+	list = *blst;
+	if (list)
+	{
+		while (list->next)
+			list = list->next;
+		list->next = ft_lstnew(content, content_size);
+	}
+	else
+		*blst = ft_lstnew(content, content_size);
+}
